b1bfda39999afb52ca18c601e2e2768ad7413e20
joe99peace <jjcusey@gmail.com>
2017-12-02 15:56:01 -0500

    Add Map

--- src/org/cusey/Collection/MainCollection.java
+++ src/org/cusey/Collection/MainCollection.java
@@ -9,7 +9,7 @@ public class MainCollection {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("***** Java ArrayList class *****");
+		System.out.println("***** List *****");
 		
 		ArrayList<String> bandMembers =new ArrayList<String>();
 		bandMembers.add("Tom"); 
@@ -21,7 +21,22 @@ public class MainCollection {
 	   Iterator<String> itr = bandMembers.iterator();  
 	   while(itr.hasNext()){  
 	    System.out.println(itr.next());  
-	   } 
+	   }
+
+		System.out.println("***** Map *****");
+
+
+		Map<String, Integer> items = new HashMap<>();
+		items.put("A", 10);
+		items.put("B", 20);
+		items.put("C", 30);
+		items.put("D", 40);
+		items.put("E", 50);
+		items.put("F", 60);
+
+		for (Map.Entry<String, Integer> entry : items.entrySet()) {
+			System.out.println("Item : " + entry.getKey() + " Count : " + entry.getValue());
+		}
 
 		
 	}
